81092820ca6ade7f28993640d5fa98e39e208195
Kai Hempel <kai.hempel@kuweh.de>
2017-11-12 21:10:00 +0100

    Payload test

--- src/test/java/de/kuweh/jwt/payload/PayloadTest.java
+++ src/test/java/de/kuweh/jwt/payload/PayloadTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class PayloadTest {
 
     @Test
-    public void testAddStringField() {
+    void addStringField() {
 
         StringField field = new StringField("sub", "1234567890");
         Payload payload = new Payload();
@@ -21,6 +21,46 @@ class PayloadTest {
         try {
 
             assertEquals("1234567890", fields.get(0).getValue());
+
+        } catch (FieldEmptyException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void addIntegerField() {
+
+        IntegerField field = new IntegerField("id", 123);
+        Payload payload = new Payload();
+
+        payload.add(field);
+
+        ArrayList<FieldInterface> fields = payload.getData();
+
+        try {
+
+            IntegerField current = (IntegerField) fields.get(0);
+            assertEquals(123, current.getValue().intValue());
+
+        } catch (FieldEmptyException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void addBooleanField() {
+
+        BooleanField field = new BooleanField("admin", true);
+        Payload payload = new Payload();
+
+        payload.add(field);
+
+        ArrayList<FieldInterface> fields = payload.getData();
+
+        try {
+
+            BooleanField current = (BooleanField) fields.get(0);
+            assertTrue(current.getValue());
 
         } catch (FieldEmptyException e) {
             e.printStackTrace();
