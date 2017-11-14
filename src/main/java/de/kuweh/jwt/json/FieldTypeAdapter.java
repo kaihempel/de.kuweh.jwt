75f0b1560595a035f8534c269a6e677651de5e39
Kai Hempel <kai.hempel@kuweh.de>
2017-11-14 08:38:51 +0100

    Debug code removed

--- src/main/java/de/kuweh/jwt/json/FieldTypeAdapter.java
+++ src/main/java/de/kuweh/jwt/json/FieldTypeAdapter.java
@@ -4,14 +4,12 @@ import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 import de.kuweh.jwt.json.fields.FieldJsonInterface;
-
 import java.io.IOException;
 
 public class FieldTypeAdapter extends TypeAdapter<FieldJsonInterface> {
 
     @Override
     public void write(JsonWriter jsonWriter, FieldJsonInterface field) throws IOException {
-        System.out.println(field.getClass());
         field.addValueToJsonWriter(jsonWriter);
     }
 
