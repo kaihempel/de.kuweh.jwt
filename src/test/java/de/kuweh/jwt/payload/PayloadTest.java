f00b576330ee528552a0af482fda26fa82dfcde7
Kai Hempel <kai.hempel@kuweh.de>
2017-11-14 08:35:46 +0100

    Payload renderable as json

--- src/test/java/de/kuweh/jwt/payload/PayloadTest.java
+++ src/test/java/de/kuweh/jwt/payload/PayloadTest.java
@@ -1,8 +1,11 @@
 package de.kuweh.jwt.payload;
 
+import com.google.gson.GsonBuilder;
+import de.kuweh.jwt.json.FieldTypeAdapter;
+import de.kuweh.jwt.json.fields.*;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,16 +14,15 @@ class PayloadTest {
     @Test
     void addStringField() {
 
-        StringField field = new StringField("sub", "1234567890");
+        StringField field = new StringField("1234567890");
         Payload payload = new Payload();
 
-        payload.add(field);
+        payload.add("sub", field);
 
-        ArrayList<FieldInterface> fields = payload.getData();
+        HashMap<String, FieldInterface> fields = payload.getData();
 
         try {
-
-            assertEquals("1234567890", fields.get(0).getValue());
+            assertEquals("1234567890", fields.get("sub").getValue());
 
         } catch (FieldEmptyException e) {
             e.printStackTrace();
@@ -30,16 +32,16 @@ class PayloadTest {
     @Test
     void addIntegerField() {
 
-        IntegerField field = new IntegerField("id", 123);
+        IntegerField field = new IntegerField(123);
         Payload payload = new Payload();
 
-        payload.add(field);
+        payload.add("id", field);
 
-        ArrayList<FieldInterface> fields = payload.getData();
+        HashMap<String, FieldInterface> fields = payload.getData();
 
         try {
 
-            IntegerField current = (IntegerField) fields.get(0);
+            IntegerField current = (IntegerField) fields.get("id");
             assertEquals(123, current.getValue().intValue());
 
         } catch (FieldEmptyException e) {
@@ -50,20 +52,38 @@ class PayloadTest {
     @Test
     void addBooleanField() {
 
-        BooleanField field = new BooleanField("admin", true);
+        BooleanField field = new BooleanField(true);
         Payload payload = new Payload();
 
-        payload.add(field);
+        payload.add("admin", field);
 
-        ArrayList<FieldInterface> fields = payload.getData();
+        HashMap<String, FieldInterface> fields = payload.getData();
 
         try {
 
-            BooleanField current = (BooleanField) fields.get(0);
+            BooleanField current = (BooleanField) fields.get("admin");
             assertTrue(current.getValue());
 
         } catch (FieldEmptyException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    void toJsonString() {
+
+        GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(StringField.class, new FieldTypeAdapter());
+        builder.registerTypeAdapter(IntegerField.class, new FieldTypeAdapter());
+        builder.registerTypeAdapter(BooleanField.class, new FieldTypeAdapter());
+
+        String expected = "{\"sub\":\"1234567890\",\"name\":\"John Doe\",\"admin\":true}";
+
+        Payload payload = new Payload();
+        payload.addString("sub", "1234567890");
+        payload.addString("name", "John Doe");
+        payload.addBoolean("admin", true);
+
+        assertEquals(expected, payload.toJson(builder));
     }
 }
