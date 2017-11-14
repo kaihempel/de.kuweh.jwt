7e01bbf3904dd555c7d6c0e0eeeeb39a73ffbdab
Kai Hempel <kai.hempel@kuweh.de>
2017-11-14 08:35:16 +0100

    Json render implementend

---
+++ src/test/java/de/kuweh/jwt/json/JsonTest.java
@@ -0,0 +1,18 @@
+package de.kuweh.jwt.json;
+
+import de.kuweh.jwt.payload.Payload;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JsonTest {
+
+    @Test
+    void encode() {
+
+        Payload payload = new Payload();
+
+        assertEquals("{}", Json.encode(payload));
+    }
+
+}
